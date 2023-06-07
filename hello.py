22345b62a6845a5c7f874d39081467fe52831bc1
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-07 18:37:59 +0530

    Added input method and formatted string

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello World!")
+
+#Now we will ask for input from the user 
+a = input("Enter your name : ") 
+print("Hello" , a , "!") 
+
+#Now we will add the formatted string 
+country = input("Country : ") 
+
+print(f"Oh You are from {country}!")
