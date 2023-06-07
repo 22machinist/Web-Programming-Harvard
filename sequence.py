8ebfc8e9e81152d00c71c7634150dc36dd5e5b49
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-08 03:30:11 +0530

    Added List

--- sequence.py
+++ sequence.py
@@ -18,7 +18,3 @@ coordinateX = 10.0
 coordinateY = 20.0 
 
 coordinate = (10.0 , 20.0)
-
-#Now we will add some code that will ressembles the coordintaes asked by user 
-
-coordinatez = int(input("Enter coordinate 1 : "))
