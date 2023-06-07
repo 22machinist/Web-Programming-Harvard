71785d702199d47226c92a1c108ef4d7f34ee38e
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-07 23:17:38 +0530

    Edited

---
+++ sequence.py
@@ -0,0 +1,16 @@
+name = "Harry"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+
+#Now we will add python list 
+names = ["Harry" , "Hermoine" , "Rudra"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+
