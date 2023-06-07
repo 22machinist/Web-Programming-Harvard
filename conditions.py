109d9515dad9e1351c8c354542ad8aca20d90b11
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-07 18:52:19 +0530

    Added if condition

---
+++ conditions.py
@@ -0,0 +1,10 @@
+#This file carries the first if condition
+
+n = int(input("Enter the number : "))
+
+if n > 0 :
+    print("Number is Positive")
+elif n < 0 :
+    print("Number is negative")
+else :
+    print("Number is zero")
