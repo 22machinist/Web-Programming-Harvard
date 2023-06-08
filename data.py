ef5de89e3538beeef5c53c35c3469be0bb02c597
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-08 09:38:26 +0530

    Added the properties

--- data.py
+++ data.py
@@ -10,3 +10,21 @@ names.append("Brock")
 names.sort()
 
 print("After Adding" , names)
+
+#Now we will create empty set 
+s = set() 
+
+#Now we will add the elements to the set defined above 
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+
+print(s)
+
+
+#Now we will remove the element from the set by using .remove() Built in function 
+s.remove(3) 
+
+# Now we will find the length of set using len() function
+print(f"Length of the given set is {len(s)}")
