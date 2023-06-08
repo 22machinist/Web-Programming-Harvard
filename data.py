b86e99aca837c90ea95d07dc68fc4290fb0348a6
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-08 09:05:45 +0530

    Added Lists

---
+++ data.py
@@ -0,0 +1,12 @@
+#Created the list
+names = ["Harry" , "Hermoine" , "Jenny" , "Wales"]
+print(names) 
+
+#Now we will use pyton built-in append function to add element to the list 
+
+names.append("Brock")
+
+#Now we will use built-in sort() python function to sort the list 
+names.sort()
+
+print("After Adding" , names)
