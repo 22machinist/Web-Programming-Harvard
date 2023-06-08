62a78e30087e12934dbd47d20750f8fddc3d017d
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-08 09:56:31 +0530

    last comit

---
+++ loops.py
@@ -0,0 +1,16 @@
+#We will create our first for loop 
+for i in range(6) :
+    print(i)
+
+#Using loops in list
+names = ["HArry" , "Brock " , "Rohit"]
+
+for name in names :
+    print(name)
+
+# Now we will loop in string 
+
+word = "Ashutosh Sharma"
+
+for char in  word : 
+    print(char)
