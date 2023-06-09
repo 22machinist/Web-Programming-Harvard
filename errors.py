336f937513697af483546934dad10445b5bb7398
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-09 10:50:13 +0530

    Handling the ValuError

--- errors.py
+++ errors.py
@@ -5,9 +5,12 @@ import sys
 
 #taking input from user 
 
-x = int(input("x: "))
-y = int(input("y: ")) 
-
+try :
+    x = int(input("x: "))
+    y = int(input("y: ")) 
+except ValueError :
+    print("Error ! Invalid Input")               #When a character is added on the place of int
+    sys.exit(1)
 try :
     result = x / y 
 except ZeroDivisionError :
